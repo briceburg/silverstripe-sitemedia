c0446c0982083d8ae7e7ac11253adea431930c80
Brice Burgess <nesta@iceburg.net>
2011-03-01 02:17:52 -0600

    removed duplicate decorators

--- code/SiteMediaPage.php
+++ code/SiteMediaPage.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * An example SiteTree type that will host all media uploaded through the 
+ *   SiteMedia module. This provides, for instance, a single page where 
+ *   visitors can view all the non-private videos and photos added to pages
+ *   and dataobjects.
+ *
+ */
 class SiteMediaPage extends Page {
 	static $db = array(
 		'MediaTypes'	=>	'Varchar(255)',
